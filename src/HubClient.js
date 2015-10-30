555a493f529a19b66f523133769631cfc59f8620
alanskas <brandon.alanskas@rovisys.com>
2015-10-30 16:10:16 -0400

    Began implementation of the HubProxy and HubClient. All tests pass. Addition towards issue #6

--- src/HubClient.js
+++ src/HubClient.js
@@ -12,5 +12,22 @@ export default class HubClient extends Client{
   constructor(options){
     super(options);
     this._config = Object.assign({}, HUB_CLIENT_CONFIG_DEFAULTS, options || {});
+    // Object to store hub proxies for this connection
+    this._proxies = {};
+    this._invocationCallbackId = 0;
+    this._invocationCallbacks = {};
+  }
+
+  _expandClientHubInvocation(compressedClientHubInvocation) {
+    return {
+      Hub: compressedClientHubInvocation.H,
+      Method: compressedClientHubInvocation.M,
+      Args: compressedClientHubInvocation.A,
+      State: compressedClientHubInvocation.S
+    };
+  }
+  createHubProxy(hubName) {
+    const hubNameLower = hubName.toLowerCase();
+    return proxy = this._proxies[hubNameLower] || (this._proxies[hubNameLower] = new HubProxy(this, hubNameLower));
   }
 }
