79bf66ac947cdee8e137a915acddfbb2d34247b4
alanskas <brandon.alanskas@rovisys.com>
2015-11-02 12:49:16 -0500

    Implemented logic to invoke client-side methods via a server-sent request. Addition towards issue #6

--- src/HubClient.js
+++ src/HubClient.js
@@ -1,6 +1,7 @@
 import Client, {CLIENT_CONFIG_DEFAULTS} from './Client';
+import {isUndefined, extend} from 'lodash';
 import HubProxy from './HubProxy';
-
+import Protocol from './Protocol';
 
 
 export const HUB_CLIENT_CONFIG_DEFAULTS = {
@@ -16,10 +17,35 @@ export default class HubClient extends Client {
     this.proxies = {};
     this.invocationCallbackId = 0;
     this.invocationCallbacks = {};
+
+    this.received(function(minData) {
+      let data, foundProxy;
+      if(!minData) {
+        return;
+      }
+      data = Protocol.expandClientHubInvocation(minData);
+      //Search proxies for message's hubProxy.
+      this._logger.info(`Message received. Looking for ${data.Hub} proxy to invoke client-side method.`);
+      for(let proxy in this.proxies){
+        if(proxy._hubName === data.Hub) {
+          proxy.invoke(data.Method, data);
+          foundProxy = true;
+        } else {
+          foundProxy = false;
+        }
+      }
+      //Proxy not found for hub, creating one..
+      if(!foundProxy){
+        this._logger.info(`HubProxy for ${data.Hub} not found. Generating proxy...`);
+        this.proxies[data.Hub] = createHubProxy(data.Hub);
+        this.proxies[data.Hub].invoke(data.Hub, data);
+      }
+    });
   }
 
   createHubProxy(hubName) {
     const hubNameLower = hubName.toLowerCase();
     return proxy = this.proxies[hubNameLower] || (this.proxies[hubNameLower] = new HubProxy(this, hubNameLower));
   }
+
 }
