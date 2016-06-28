fb949ff91f7fdf7a3fa47afdd49974675d8668bc
raiseandfall <druidev+github@gmail.com>
2016-06-28 14:32:01 -0500

    [FEAT] Added export of client & connection states to avoid duplicating codes everywhere

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import Client from './Client';
+import { CLIENT_STATES as CS, CONNECTION_STATES as CCS } from './Constants';
+
+export const CLIENT_STATES = CS;
+export const CONNECTION_STATES = CCS;
 
 export default Client;
